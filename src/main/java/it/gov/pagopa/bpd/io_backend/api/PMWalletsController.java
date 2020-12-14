7fd1df7563da8a54cbf9773a892ec50548107e60
alecastagno <alessandro.castagno.sia@gmail.com>
2020-12-14 18:27:29 +0100

    Added hashed token list to NPWallet

--- src/main/java/it/gov/pagopa/bpd/io_backend/api/PMWalletsController.java
+++ src/main/java/it/gov/pagopa/bpd/io_backend/api/PMWalletsController.java
@@ -1,9 +1,7 @@
 package it.gov.pagopa.bpd.io_backend.api;
 
 import it.gov.pagopa.bpd.io_backend.model.wallets.dto.WalletNPInput;
-import it.gov.pagopa.bpd.io_backend.model.wallets.resource.LEWallet;
 import it.gov.pagopa.bpd.io_backend.model.wallets.resource.NPWallet;
-import it.gov.pagopa.bpd.io_backend.model.wallets.resource.WalletsResource;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -29,7 +28,7 @@ public class PMWalletsController implements PMWalletsApi {
 
     @Override
     public ResponseEntity<List<NPWallet>> getHpan(@Valid @NotBlank @NotNull String pan) {
-        String hpan = DigestUtils.sha256Hex(pan+salt);
+        String hpan = DigestUtils.sha256Hex(pan + salt);
         return new ResponseEntity<>(
                 Collections.singletonList(NPWallet.builder().hashPan(hpan).build()),
                 HttpStatus.OK);
@@ -37,10 +36,18 @@ public class PMWalletsController implements PMWalletsApi {
 
     @Override
     public ResponseEntity<NPWallet> saveHpan(@Valid @NotNull WalletNPInput walletNPInput) {
-        String hpan = DigestUtils.sha256Hex(walletNPInput.getPan()+salt);
+        String hpan = DigestUtils.sha256Hex(walletNPInput.getPan() + salt);
+        List<String> htokenList = new ArrayList<>();
+
+        if (walletNPInput.getTokenList() != null) {
+            for (String token : walletNPInput.getTokenList()) {
+                String htoken = DigestUtils.sha256Hex(token + salt);
+                htokenList.add(htoken);
+            }
+        }
         return new ResponseEntity<>(
-                NPWallet.builder().hashPan(hpan).build(),
-                HttpStatus.OK);    
+                NPWallet.builder().hashPan(hpan).hashTokenList(htokenList).build(),
+                HttpStatus.OK);
     }
 
 
