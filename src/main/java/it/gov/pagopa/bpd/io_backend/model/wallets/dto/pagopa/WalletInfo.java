55a026a96d30a72fd5cb8e8e6e671142f598fc13
anttorre <antonio.torre@everis.com>
2021-07-13 11:37:29 +0200

    added PM service openApi: fixed WalletInfo walletType reference

--- src/main/java/it/gov/pagopa/bpd/io_backend/model/wallets/dto/pagopa/WalletInfo.java
+++ src/main/java/it/gov/pagopa/bpd/io_backend/model/wallets/dto/pagopa/WalletInfo.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.PROPERTY,
+        include = JsonTypeInfo.As.EXTERNAL_PROPERTY,
         property = "walletType")
 @JsonSubTypes({
         @JsonSubTypes.Type(value = WalletCardInfoInput.class, name = "Card"),
