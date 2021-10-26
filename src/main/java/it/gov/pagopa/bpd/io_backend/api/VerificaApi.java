6ce8bbd6d126e1125d9406c0e56043b6c8a67a70
daniloMessinaSia <danilo.messina.sia@gmail.com>
2021-10-26 17:21:30 +0200

    Add RequestMapping in validationPiva's API

--- src/main/java/it/gov/pagopa/bpd/io_backend/api/VerificaApi.java
+++ src/main/java/it/gov/pagopa/bpd/io_backend/api/VerificaApi.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import io.swagger.annotations.Api;
 import it.gov.pagopa.bpd.io_backend.model.ade.VerificaPartitaIva;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Api("AdE_verifica_CF")
+@RequestMapping("/WsAnagraficaDL78Web/services/VerificaAnagraficaDL78InterfaceEndpoint")
 @Validated
 public interface VerificaApi {
 
