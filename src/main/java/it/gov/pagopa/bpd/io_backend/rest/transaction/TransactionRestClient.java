94c2146930c28ae54464fa212f3ead6bb094133b
daniloMessinaSia <danilo.messina.sia@gmail.com>
2021-10-28 17:00:37 +0200

    update input request

--- src/main/java/it/gov/pagopa/bpd/io_backend/rest/transaction/TransactionRestClient.java
+++ src/main/java/it/gov/pagopa/bpd/io_backend/rest/transaction/TransactionRestClient.java
@@ -1,17 +1,14 @@
 package it.gov.pagopa.bpd.io_backend.rest.transaction;
 
-import io.swagger.annotations.ApiParam;
 import it.gov.pagopa.bpd.io_backend.rest.model.transaction.PosTransactionRequestDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
 
 /**
  * Customer Rest Client
@@ -22,9 +19,6 @@ public interface TransactionRestClient {
     @PostMapping(value = "${rest-client.transaction.request.url}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     String createPosTransaction(
-            @ApiParam(value = "${swagger.transaction.transactionId}", required = true)
-            @PathVariable("transactionId") @Valid @NotBlank
-                    String transactionId,
             @Valid @RequestBody PosTransactionRequestDTO request
     );
 }
