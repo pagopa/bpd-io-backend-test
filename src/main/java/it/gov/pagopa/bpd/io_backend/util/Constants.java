224d352e0688a132e67cc686fb72b3a971c3ce00
Francesco Esposito <francesco12.esposito@everis.nttdata.com>
2021-10-28 15:56:29 +0200

    CEN-868: fix some sonar issues

--- src/main/java/it/gov/pagopa/bpd/io_backend/util/Constants.java
+++ src/main/java/it/gov/pagopa/bpd/io_backend/util/Constants.java
@@ -1,5 +1,9 @@
 package it.gov.pagopa.bpd.io_backend.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Constants {
 
     public static final String FISCAL_CODE_REGEX = "^([A-Za-z]{6}[0-9lmnpqrstuvLMNPQRSTUV]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9lmnpqrstuvLMNPQRSTUV]{2}[A-Za-z]{1}[0-9lmnpqrstuvLMNPQRSTUV]{3}[A-Za-z]{1})$";
